416ac2129616736a42b07986f7ebdb9402345dbd
hefju <hefju@live.cn>
2014-07-01 22:08:02 +0800

    改用gin提供服务
    今日了解到gin的性能比较好, 于是就以此为基础建立服务

---
+++ src/PPMThird.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+			c.String(200, "Hello, I'm PPMThird.")
+		})
+
+	r.GET("/user/:name", func(c *gin.Context) {
+			name := c.Params.ByName("name")
+			message := "Hello " + name
+			//c.String(200, message)
+//			c.XML(200,message)
+			c.JSON(200,message)
+
+		})
+
+	// Listen and server on 0.0.0.0:8080
+	r.Run(":8080")
+}
